82f18ac7ec2fedf77e400d8948fa5f637a06d61f
Daniel Ryland <rylandcs@gmail.com>
2023-07-07 17:36:35 -0500

    added styling and javascript for modals

--- js/main.js
+++ js/main.js
@@ -114,10 +114,10 @@ for (const link of filterLink) {
 
 
 
-//Full Site Modal "open buttons"
+//Modal/Full Site Modal "open buttons"
 for (const elm of openModal) {
   elm.addEventListener('click', function() {
-    const modalId = this.dataset.open;
+    const modalId = this.dataset.open; //unique ID
     document.getElementById(modalId).classList.add(isVisible);
   })
 }
@@ -127,3 +127,18 @@ for (const elm of closeModal) {
     this.parentElement.parentElement.parentElement.classList.remove(isVisible);
   })
 }
+
+// Individual Modals
+document.addEventListener('click', (e) => {
+  console.log(e.target, document.querySelector('.modal.is-visible'));
+  if (e.target === document.querySelector('.modal.is-visible')) {
+    document.querySelector('.modal.is-visible').classList.remove(isVisible);
+  }
+})
+
+document.addEventListener('keyup', (e) => {
+  console.log(e.key);
+  if (e.key === 'Escape') {
+    document.querySelector('.modal.is-visible').classList.remove(isVisible);
+  }
+})
